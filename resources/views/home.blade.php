c59903a1041bbd4a343debc646f486f03091775d
Jorge Araya <jorgearaya@Jorges-MacBook-Air.local>
2024-08-26 21:45:55 -0600

    add home template - nav

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<x-guest-layout>
+    <div class="max-w-screen-xl mx-auto px-5">
+        <header class="flex flex-col lg:flex-row justify-between items-center my-5">
+            <div class="flex w-full lg:w-auto items-center justify-between">
+                <a href="/" class="text-lg">
+                    <span class="font-bold text-slate-800">Lara</span><span class="text-slate-500">vlog</span> 
+                </a>
+            </div>
+            <nav class="w-full lg:w-auto mt-2 lg:flex lg:mt-0">
+            <ul class="flex flex-col lg:flex-row lg:gap-3">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#" class="rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 px-4 py-2 bg-black text-white hover:bg-gray-800  border-2 border-transparent">Contact</a>
+                </li>
+            </ul>
+            </nav>
+        </header>
+    </div>
+</x-guest-layout>
